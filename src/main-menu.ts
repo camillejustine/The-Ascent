2d27c1b2016a9756ce2447680250997c616dcfe3
Intradastingly <asaf_lackgren@hotmail.com>
2020-12-15 15:16:44 +0100

    mainMenu start button add

--- src/main-menu.ts
+++ src/main-menu.ts
@@ -1,5 +1,27 @@
-class MainMenu {
-
+    class MainMenu {
+        //private game: InterfaceGameState;
+        //isGameRunning: boolean;
+        private startButton: HTMLElement;
+        private aboutButton: HTMLElement;
+        private isGameRunning: boolean;
+        
+        constructor() {
+            this.isGameRunning = false;
+            this.startButton = document.getElementById('button');
+        }
+        
+        public update() {
+            this.startButton.onclick = this.gameState;
+        }
+        
+        public gameState() {
+            this.isGameRunning = true;
+            if(this.isGameRunning){
+                gameFrame.draw();
+                gameFrame.update();
+            } 
+        }
+    }
     /* public constructor() {}
     
     private gameFrame() {}
