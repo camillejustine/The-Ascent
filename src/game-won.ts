484db286e5ba3260bd68de2bf1ff0d05108ca911
Josefin Enerlöv <your-email-address>
2021-01-07 10:31:34 +0100

    buttons

--- src/game-won.ts
+++ src/game-won.ts
@@ -1,11 +1,28 @@
 class Gamewon {
+    private div: p5.Element;
 
-    /* public constructor() {}
+    public constructor() {
+        this.div = createElement('main-menu');
 
-    public update() {}
+        const button1 = createElement('button');
+        button1.addClass('button');
+        button1.html('Click here');
+        button1.mousePressed(this.restartGame);
+
+        const button2 = createElement('button');
+        button2.addClass('button');
+        button2.html('Click here');
+        button2.mousePressed(this.backToMain);
+
+
+    }
+
+    //public update() {}
     
-    private restartGame() {}
+    private restartGame() {
+        this.div.show()
+    }
     
     private backToMain() {}
- */
+
 }
