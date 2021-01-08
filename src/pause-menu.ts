a5ffd3c3cc2590732384484537f658b3a93f7864
Camille Delas Llagas <camilledelasllagas@Camilles-MBP.lan>
2021-01-08 15:44:16 +0100

    removed old code

--- src/pause-menu.ts
+++ src/pause-menu.ts
@@ -1,51 +1,37 @@
 class PauseMenu {
-        private div: p5.Element;
-        private restartGameCallback: iGameState;
+  private div: p5.Element;
+  private restartGameCallback: iGameState;
 
-        public pauseMenu: any;
-        public backToMain: any;
-        //private aboutButton: any;
-        
-        public constructor(restartGameCallback: iGameState) {
-            this.restartGameCallback = restartGameCallback;
-            this.div = createElement('main-menu');
-    
-            const resumeButton = createElement('button');
-            resumeButton.addClass('button');
-            resumeButton.html('Restart');
-            resumeButton.mousePressed(() => this.resumeGame);
-    
-            const backToMain = createElement('button');
-            backToMain.addClass('button');
-            backToMain.html('Back To Main');
-            backToMain.mousePressed(() => this.backToMain());
-    
-            this.div.child(resumeButton);
-            this.div.child(backToMain);
-        }
-      
-        public update() {
-         this.keyPressed();
-        }
+  public pauseMenu: any;
+  public backToMain: any;
+  //private aboutButton: any;
+  
+  public constructor(restartGameCallback: iGameState) {
+      this.restartGameCallback = restartGameCallback;
+      this.div = createElement('main-menu');
 
-        public keyPressed() {
-            if (keyCode === 32) {
-              this.game.gameState = 'pauseMenu';
-            } 
-            return false;      
-        }
+      const resumeButton = createElement('button');
+      resumeButton.addClass('button');
+      resumeButton.html('Restart');
+      resumeButton.mousePressed(() => this.resumeGame);
 
+      const backToMain = createElement('button');
+      backToMain.addClass('button');
+      backToMain.html('Back To Main');
+      backToMain.mousePressed(() => this.backToMain());
 
-        // public unpause() {
-        //   if (keyCode === DOWN_ARROW) {
-        //     this.game.gameState = 'running'; 
-        //     console.log('test')
-        //   }
-        // }
-        /* public keyReleased() {
-          if (keyCode === 32) {
-              this.game.gameState = 'running';
-          } */
+      this.div.child(resumeButton);
+      this.div.child(backToMain);
+  }
 
-}
+  public update() {
+   this.keyPressed();
+  }
+
+  public keyPressed() {
+      if (keyCode === 32) {
+        this.game.gameState = 'pauseMenu';
+      } 
+      return false;      
+  }
 
