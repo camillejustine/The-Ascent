8463ef5aaa8033ac04e831cd506688c4404a01b3
supertramps <alexander.liljedahl@live.se>
2021-01-11 09:51:23 +0100

    Health can no longer be less than 0

--- src/submarine.ts
+++ src/submarine.ts
@@ -7,8 +7,8 @@ class Submarine {
   public obstacle: ObstacleArray;
   public allObjectsArray: Array<any>;
   private deletedArray: Array<any>;
-  
-  constructor(obstacle: ObstacleArray){
+
+  constructor(obstacle: ObstacleArray) {
     this.obstacle = obstacle;
     this.control = new Control();
     this.subPositionX = 0;
@@ -23,12 +23,14 @@ class Submarine {
     this.angle = this.control.getAngle();
     this.collisionHullDamage();
     this.pickUpHullFix();
-    this.allObjectsArray = this.obstacle.obstacles.concat(this.obstacle.powerUps)
+    this.allObjectsArray = this.obstacle.obstacles.concat(
+      this.obstacle.powerUps
+    );
     //console.log(this.hullHealth)
-    }
+  }
 
   public draw() {
-    this.update()
+    this.update();
     this.control.update();
     this.subPositionX = this.control.getPositionX();
     this.subPositionY = this.control.getPositionY();
@@ -38,27 +40,40 @@ class Submarine {
     rotate(this.angle);
     imageMode(CENTER);
     image(subImage, 0, 0, 35, 150);
-    pop(); 
-  }
-  
-  public collisionHullDamage(){
-    for(let i = 0; i < this.allObjectsArray.length; i++){
-        if(this.allObjectsArray[i].collision && this.allObjectsArray[i].id === 'iceberg'){
-          this.hullHealth = this.hullHealth - 0.25;
-        }if(this.allObjectsArray[i].collision && this.allObjectsArray[i].id === 'mine'){
-          this.hullHealth = 0;
-      } 
-    } 
+    pop();
   }
 
-  public pickUpHullFix(){
-    for(let i = 0; i < this.allObjectsArray.length; i++){
-      if(this.allObjectsArray[i].collision && this.allObjectsArray[i].id === 'supplyBox'){
+  public collisionHullDamage() {
+    for (let i = 0; i < this.allObjectsArray.length; i++) {
+      if (
+        this.allObjectsArray[i].collision &&
+        this.allObjectsArray[i].id === "iceberg" &&
+        this.hullHealth > 0
+      ) {
+        this.hullHealth = this.hullHealth - 0.25;
+      }
+
+      if (
+        this.allObjectsArray[i].collision &&
+        this.allObjectsArray[i].id === "mine" &&
+        this.hullHealth > 0
+      ) {
+        this.hullHealth = 0;
+      }
+    }
+  }
+
+  public pickUpHullFix() {
+    for (let i = 0; i < this.allObjectsArray.length; i++) {
+      if (
+        this.allObjectsArray[i].collision &&
+        this.allObjectsArray[i].id === "supplyBox"
+      ) {
         this.hullHealth += 0.25;
-        if(this.hullHealth >= 100){
+        if (this.hullHealth >= 100) {
           this.hullHealth = 100;
         }
-        this.allObjectsArray.splice(i,1)
+        this.allObjectsArray.splice(i, 1);
       }
     }
   }
