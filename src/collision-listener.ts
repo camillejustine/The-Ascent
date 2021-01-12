453b0fc994058af1ebeee2a72ec5c5f31a8af297
Intradastingly <asaf_lackgren@hotmail.com>
2021-01-12 15:46:28 +0100

    collisions working

--- src/collision-listener.ts
+++ src/collision-listener.ts
@@ -133,8 +133,6 @@ class CollisionListener {
       }
 
       public rotatePointAroundCenter(point: p5.Vector, center: p5.Vector, angle: number): p5.Vector {​​​​
-        //angle in radions 
-        /** Rotates a point around another center point, will return a new point */
         const angleToCenter = Math.atan2(point.y - center.y, point.x - center.x);
         const distToCenter = center.dist(point);
         return p5.Vector.fromAngle(angleToCenter + angle, distToCenter).add(center);
