4fd465174d9f424a7b96ccdd982a883add38097f
Intradastingly <asaf_lackgren@hotmail.com>
2021-01-12 15:37:00 +0100

    collisions work on all circles

--- src/collision-listener.ts
+++ src/collision-listener.ts
@@ -40,133 +40,78 @@ class CollisionListener {
       
       public submarineCollisions(){
         let circleArray = [
-          {"x": this.cx, "y": this.cy - 60, "r": this.r},
-          {"x": this.cx, "y": this.cy - 30, "r": this.r},
-          {"x": this.cx, "y": this.cy , "r": this.r},
-          {"x": this.cx, "y": this.cy + 30, "r": this.r},
-          {"x": this.cx, "y": this.cy + 60, "r": this.r} 
+          {"x1": this.cx, "y1": this.cy - 60, "r": this.r},
+          {"x2": this.cx, "y2": this.cy - 30, "r": this.r},
+          {"x3": this.cx, "y3": this.cy , "r": this.r},
+          {"x4": this.cx, "y4": this.cy + 30, "r": this.r},
+          {"x5": this.cx, "y5": this.cy + 60, "r": this.r} 
         ]; 
           for(let i = 0; i < this.allObjectsArray.allObjects.length; i++){
            for(let j = 0; j < circleArray.length; j++){
-              let position = createVector(circleArray[j].x,circleArray[j].y);
+              let position1 = createVector(circleArray[0].x1,circleArray[0].y1);
+              let position2 = createVector(circleArray[1].x2,circleArray[1].y2);
+              let position3 = createVector(circleArray[2].x3,circleArray[2].y3);
+              let position4 = createVector(circleArray[3].x4,circleArray[3].y4);
+              let position5 = createVector(circleArray[4].x5,circleArray[4].y5);
               let center = createVector(this.cx,this.cy);
-              let rotate = this.rotatePointAroundCenter(position, center,this.angle)
-              circle(rotate.x, rotate.y, circleArray[j].r)
-                let collision = this.detect(
-                rotate.x, 
-                rotate.y, 
-                circleArray[j].r, 
-                this.allObjectsArray.allObjects[i].x, 
-                this.allObjectsArray.allObjects[i].y, 
-                this.allObjectsArray.allObjects[i].r/2
-                );
-                if (collision) {
+              let rotate1 = this.rotatePointAroundCenter(position1, center,this.angle)
+              let rotate2 = this.rotatePointAroundCenter(position2, center,this.angle)
+              let rotate3 = this.rotatePointAroundCenter(position3, center,this.angle)
+              let rotate4 = this.rotatePointAroundCenter(position4, center,this.angle)
+              let rotate5 = this.rotatePointAroundCenter(position5, center,this.angle)  
+              circle(rotate1.x, rotate1.y, circleArray[j].r)
+              circle(rotate2.x, rotate2.y, circleArray[j].r)
+              circle(rotate3.x, rotate3.y, circleArray[j].r)
+              circle(rotate4.x, rotate4.y, circleArray[j].r)
+              circle(rotate5.x, rotate5.y, circleArray[j].r)
+                let collision1 = this.detect(
+                  rotate1.x, 
+                  rotate1.y, 
+                  circleArray[j].r, 
+                  this.allObjectsArray.allObjects[i].x, 
+                  this.allObjectsArray.allObjects[i].y, 
+                  this.allObjectsArray.allObjects[i].r/2
+                  );
+                let collision2 = this.detect(
+                  rotate2.x, 
+                  rotate2.y, 
+                  circleArray[j].r, 
+                  this.allObjectsArray.allObjects[i].x, 
+                  this.allObjectsArray.allObjects[i].y, 
+                  this.allObjectsArray.allObjects[i].r/2
+                  );
+                let collision3 = this.detect(
+                  rotate3.x, 
+                  rotate3.y, 
+                  circleArray[j].r, 
+                  this.allObjectsArray.allObjects[i].x, 
+                  this.allObjectsArray.allObjects[i].y, 
+                  this.allObjectsArray.allObjects[i].r/2
+                  );
+                let collision4 = this.detect(
+                  rotate4.x, 
+                  rotate4.y, 
+                  circleArray[j].r, 
+                  this.allObjectsArray.allObjects[i].x, 
+                  this.allObjectsArray.allObjects[i].y, 
+                  this.allObjectsArray.allObjects[i].r/2
+                  );
+                let collision5 = this.detect(
+                  rotate5.x, 
+                  rotate5.y, 
+                  circleArray[j].r, 
+                  this.allObjectsArray.allObjects[i].x, 
+                  this.allObjectsArray.allObjects[i].y, 
+                  this.allObjectsArray.allObjects[i].r/2
+                  );
+                if (collision1 || collision2 || collision3 || collision4 || collision5) {
                   this.allObjectsArray.allObjects[i].collision = true;
                   this.allObjectsArray.allObjects[i].collided = true;
                 } else {
                   this.allObjectsArray.allObjects[i].collision = false;
                 } 
               }
-            }
-            /* for(let i = 0; i < this.allObjectsArray.allObjects.length; i++){
-
-              let position = createVector(circleArray[1].x,circleArray[1].y);
-              let center = createVector(this.cx,this.cy);
-              let rotate = this.rotatePointAroundCenter(position, center,this.angle)
-
-              circle(rotate.x, 
-                rotate.y, 
-                circleArray[1].r)
-              let collision = this.detect(
-                rotate.x, 
-                rotate.y, 
-                circleArray[1].r, 
-                this.allObjectsArray.allObjects[i].x, 
-                this.allObjectsArray.allObjects[i].y, 
-                this.allObjectsArray.allObjects[i].r/2
-                );
-                if (collision) {
-                  this.allObjectsArray.allObjects[i].collision = true;
-                  this.allObjectsArray.allObjects[i].collided = true;
-                } else {
-                  this.allObjectsArray.allObjects[i].collision = false;
-                } 
-            }
-            for(let i = 0; i < this.allObjectsArray.allObjects.length; i++){
-
-              let position = createVector(circleArray[2].x,circleArray[2].y);
-              let center = createVector(this.cx,this.cy);
-              let rotate = this.rotatePointAroundCenter(position, center,this.angle)
-
-              circle(rotate.x, 
-                rotate.y, 
-                circleArray[2].r)
-                circle(rotate.x, 
-                  rotate.y, 
-                  circleArray[2].r)
-              let collision = this.detect(
-                rotate.x, 
-                rotate.y, 
-                circleArray[2].r, 
-                this.allObjectsArray.allObjects[i].x, 
-                this.allObjectsArray.allObjects[i].y, 
-                this.allObjectsArray.allObjects[i].r/2
-                );
-                if (collision) {
-                  this.allObjectsArray.allObjects[i].collision = true;
-                  this.allObjectsArray.allObjects[i].collided = true;
-                } else {
-                  this.allObjectsArray.allObjects[i].collision = false;
-                } 
-            }
-            for(let i = 0; i < this.allObjectsArray.allObjects.length; i++){
-
-              let position = createVector(circleArray[3].x,circleArray[3].y);
-              let center = createVector(this.cx,this.cy);
-              let rotate = this.rotatePointAroundCenter(position, center,this.angle)
-
-              circle(rotate.x, 
-                rotate.y, 
-                circleArray[3].r)
-              let collision = this.detect(
-                rotate.x, 
-                rotate.y, 
-                circleArray[3].r, 
-                this.allObjectsArray.allObjects[i].x, 
-                this.allObjectsArray.allObjects[i].y, 
-                this.allObjectsArray.allObjects[i].r/2
-                );
-                if (collision) {
-                  this.allObjectsArray.allObjects[i].collision = true;
-                  this.allObjectsArray.allObjects[i].collided = true;
-                } else {
-                  this.allObjectsArray.allObjects[i].collision = false;
-                } 
-            }
-           */  /*  for(let i = 0; i < this.allObjectsArray.allObjects.length; i++){
-
-              let position = createVector(circleArray[4].x,circleArray[4].y);
-              let center = createVector(this.cx,this.cy);
-              let rotate = this.rotatePointAroundCenter(position, center,this.angle)
-
-              circle(rotate.x, 
-                rotate.y, 
-                circleArray[4].r)
-              let collision = this.detect(
-                rotate.x, 
-                rotate.y, 
-                circleArray[4].r, 
-                this.allObjectsArray.allObjects[i].x, 
-                this.allObjectsArray.allObjects[i].y, 
-                this.allObjectsArray.allObjects[i].r/2
-                );
-                if (collision) {
-                  this.allObjectsArray.allObjects[i].collision = true;
-                  this.allObjectsArray.allObjects[i].collided = true;
-                } else {
-                  this.allObjectsArray.allObjects[i].collision = false;
-                } 
-            }  */
+          }
       }
 
       public sonarDetection(){
@@ -225,7 +170,6 @@ class CollisionListener {
       return false;
     } */
 
-
      /* let collision = this.subCollision(
             this.allObjectsArray.allObjects[i].x,
             this.allObjectsArray.allObjects[i].y,
@@ -234,11 +178,7 @@ class CollisionListener {
             this.cy,
             this.rectW,
             this.rectH
-          ); */
-
-
-    
-  
+          ); */ 
 }
 
     
